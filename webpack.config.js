9828b0597a90575913517f7da81753ab97c0924a
Jack Davis <jack.davis@akqa.com>
2017-05-04 12:03:49 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    libraryTarget: 'umd'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }],
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [{
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader",
+          options: {
+            module: true,
+            localIdentName: '[name]__[local]--[hash:base64:5]',
+            importLoaders: 1,
+          }
+        },
+        {
+          loader: "sass-loader"
+        },
+      ]
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, './src'), 'node_modules']
+  },
+  plugins: [
+    new StaticSiteGeneratorPlugin()
+  ]
+};
