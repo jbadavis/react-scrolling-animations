f732f93e5468f78a543b98cb606352d9f0968bb9
Jack Davis <jack.davis@akqa.com>
2017-05-05 13:36:45 -0700

    Small copy update

--- src/app/components/About/About.js
+++ src/app/components/About/About.js
@@ -8,8 +8,8 @@ const About = () => {
   return (
     <div className={styles.about}>
       <h6 className={styles.aboutTitle}>About</h6>
-      <p className={styles.aboutP}>Will is an award winning Barber based in Wolverhampton.</p>
-      <p className={styles.aboutP}>He is focused on providing an exemplary service to his customers</p>
+      <p className={styles.aboutP}>Will is an award-winning Barber based in Wolverhampton.</p>
+      <p className={styles.aboutP}>He is dedicated to providing exemplary service to his customers</p>
       <p className={styles.aboutP}>Please contact Will for appointments.</p>
       <img src={url} className={styles.aboutImage}/>
     </div>
