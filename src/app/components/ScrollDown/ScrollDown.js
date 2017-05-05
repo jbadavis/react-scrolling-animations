b903748fb819454b54023af72bb134bf5031e8e1
Jack Davis <jack.davis@akqa.com>
2017-05-04 18:08:42 -0700

    Scroll indicator added

---
+++ src/app/components/ScrollDown/ScrollDown.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './scrollDown.scss';
+
+const ScrollDown = () => {
+  return (
+    <h5 className={styles.scrollDown}>Scroll Down</h5>
+  );
+};
+
+export default ScrollDown;
