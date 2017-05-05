24462103b1575ec4a41e2bffd36e97394f5fa3ff
Jack Davis <jack.davis@akqa.com>
2017-05-04 21:25:40 -0700

    Scroll down transition - borders

--- src/app/components/ScrollDown/ScrollDown.js
+++ src/app/components/ScrollDown/ScrollDown.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 import styles from './scrollDown.scss';
 
 const ScrollDown = () => {
   return (
-    <h5 className={styles.scrollDown}>Scroll Down</h5>
+    <CSSTransitionGroup
+      transitionName={styles}
+      transitionAppear={true}
+      transitionAppearTimeout={2400}
+      transitionEnter={false}
+      transitionLeave={false}>
+      <h5 className={styles.scrollDown}>Scroll Down</h5>
+    </CSSTransitionGroup>
   );
 };
 
