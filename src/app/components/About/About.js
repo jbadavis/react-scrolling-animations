edcf27bb11c23072073c7252519c6461473ba9c2
Jack Davis <jack.davis@akqa.com>
2017-05-06 19:03:15 -0700

    Small copy change

--- src/app/components/About/About.js
+++ src/app/components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <div className={styles.about}>
       <h6 className={styles.aboutTitle}>About</h6>
       <p className={styles.aboutP}>Will is an award-winning Barber based in Wolverhampton.</p>
-      <p className={styles.aboutP}>He is dedicated to providing exemplary service to his customers</p>
+      <p className={styles.aboutP}>He is dedicated to providing an exemplary service to his customers</p>
       <p className={styles.aboutP}>Please contact Will for appointments.</p>
       <img src={url} className={styles.aboutImage}/>
     </div>
