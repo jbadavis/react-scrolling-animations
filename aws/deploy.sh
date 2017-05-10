d5f719ad080cbb174fb5de8ca2f9544c5dafcbf0
Jack Davis <jack.davis@akqa.com>
2017-05-10 15:34:02 -0700

    Minor script update

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,4 +1,11 @@
-rm -r dist
-webpack -p
-aws s3 sync dist/ s3://will-the-barber
+echo "Removing Previous Build\n"
 
+rm -r dist
+
+echo "\nRunning Webpack\n"
+
+webpack -p --progress
+
+echo "\nPushing to S3\n"
+
+aws s3 sync dist/ s3://will-the-barber
