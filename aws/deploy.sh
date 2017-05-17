ce13c8cdde628feb42dacdcd7771b1dd0d6d0487
Jack Davis <jack.davis@akqa.com>
2017-05-17 14:19:47 -0700

    Added gzip compression - updated deploy scripts

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,13 +1,4 @@
-echo "Removing Previous Build\n"
-
-rm -r dist
-
-echo "Done!"
-
-echo "\nRunning Webpack\n"
-
-webpack -p --progress
-
 echo "\nPushing to S3\n"
 
-aws s3 sync dist/ s3://will-the-barber
+aws s3 sync dist/ s3://will-the-barber --exclude "*.js"
+aws s3 cp --content-encoding gzip ./dist/app.bundle.js s3://will-the-barber/app.bundle.js
